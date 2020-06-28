d1fd89023504461db902ce90c25b8799bf06ef76
Ward Price <wardprice@gmail.com>
2020-06-28 15:49:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ def find_min_in_nested_arrays(src)
     element_index = 0
     smallest_temp = 100 
     while element_index < src[row_index].count do
-      if src[row_index][element_index] > smallest_temp
+      if src[row_index][element_index] < smallest_temp
         smallest_temp = src[row_index][element_index]
       end
       element_index += 1
